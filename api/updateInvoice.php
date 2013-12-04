24e4499e5ae0b594964741c40f665546ec854e9f
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-12-04 23:46:25 +0000

    import saft seems done, error handling and everything

--- api/updateInvoice.php
+++ api/updateInvoice.php
@@ -33,8 +33,8 @@ if ($invoiceNo == NULL) {
     $invoiceNo = getLastInvoiceNoPlusOne();
     $invoiceInfo['invoiceNo'] = $invoiceNo;
     $insertUrl = getCurrentPageUrl();
-    $insertUrl = substr($insertUrl, 0, strpos($insertUrl, 'api/'));
-    $insertUrl .= '/api/insertInvoice.php';
+    $insertUrl = substr($insertUrl, 0, strpos($insertUrl, 'api'));
+    $insertUrl .= 'api/insertInvoice.php';
     $response = http_post($insertUrl, array('invoice' => json_encode($invoiceInfo)));
     die($response);
 }
@@ -86,6 +86,7 @@ function getLastInvoiceNo(){
 }
 
 function getLastInvoiceNoPlusOne() {
+    // TODO handle case when there isn't any invoice!
     $invoiceNo = getLastInvoiceNo();
     $matches = array();
     preg_match('/(\d+)$/', $invoiceNo, $matches);
