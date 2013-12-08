9b005f701157a34690ab0cf5b9a04393b6b329b7
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-12-08 22:59:13 +0000

    on updating a product price, the invoices now update as well

--- api/product.php
+++ api/product.php
@@ -3,6 +3,7 @@ require_once 'search.php';
 require_once 'utilities.php';
 require_once 'update.php';
 require_once 'insert.php';
+require_once 'delete.php';
 
 function getProduct($productCode) {
     // Fetch the product we are looking for
@@ -32,24 +33,55 @@ function updateProduct($productInfo) {
 
     $table = 'Product';
     $field = 'ProductCode';
-    
     if(isset($productInfo['ProductCode']))
         $productCode = $productInfo['ProductCode'];
     else
         $productCode = NULL;
 
     $obligatoryFields = array('ProductDescription');
-    $optionalFields = array('UnitPrice', 'UnitOfMeasure');
+    $optionalFields = array('ProductCode', 'UnitPrice', 'UnitOfMeasure');
     validateFields($productInfo, $obligatoryFields, $optionalFields);
 
     if ($productCode == NULL) {
         $productCode = getLastProductCode() + 1;
         $productInfo['ProductCode'] = $productCode;
         new Insert('Product', $productInfo);
-    } else
+    } else {
+        $search = new EqualSearch('Product', 'ProductCode', array($productInfo['ProductCode']), array('*'));
+        $results = $search->getResults();
+        $oldProduct = $results[0];
         new Update($table, $productInfo, $field, $productCode);
+        updateInvoicesOnPriceChange($productInfo, $oldProduct);
+    }
 
     return getProduct($productCode);
+}
+
+function updateInvoicesOnPriceChange($productInfo, $oldProduct) {
+    if ($oldProduct['UnitPrice'] == $productInfo['UnitPrice']) {
+        return;
+    }
+    $productId = $oldProduct['ProductID'];
+    $search = new EqualSearch('InvoiceLine', 'ProductID', array($productId), array('*') );
+    $invoiceLines = $search->getResults();
+    foreach($invoiceLines as $line) {
+        // update the invoice totals
+        $invoiceInfo = array();
+        $invoiceInfo['TaxPayable'] = 0;
+        $invoiceInfo['NetTotal'] = 0;
+        $invoiceInfo['GrossTotal'] = 0;
+        new Update('Invoice', $invoiceInfo, 'InvoiceID', $line['InvoiceID']);
+
+        $search = new EqualSearch('InvoiceLine', 'InvoiceID', array($line['InvoiceID']), array('*'));
+        $newLines = $search->getResults();
+
+        new Delete('InvoiceLine', array('InvoiceID' => $line['InvoiceID']));
+        foreach ($newLines as $line) {
+            unset($line['CreditAmount']);
+            new Insert('InvoiceLine', $line);
+        }
+    }
+
 }
 
 function getLastProductCode(){
