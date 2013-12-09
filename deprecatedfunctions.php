dc83b78c8f07b7627ca248c852e09fd36abd7c90
Diogo Santos <diogo.rgsantos@gmail.com>
2013-12-09 01:23:20 +0000

    Someone decided it would be cool to use a function that does not exist on windows :< Credit is on the sprptimewin.php file.

--- deprecatedfunctions.php
+++ deprecatedfunctions.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'sprptimeWin.php';
+
 if ( false === function_exists('lcfirst') ) {
     function lcfirst( $str )
     { return (string)(strtolower(substr($str,0,1)).substr($str,1));}
